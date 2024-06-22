7551acbbd87e66e3ce332d92e3bfe4ea8f1ea584
Mark Benson <mark.benson@nih.gov>
2024-06-21 22:59:17 -0400

    script now reads dates as optional arguments, no temp files

--- fileDateIndexer.sh
+++ fileDateIndexer.sh
@@ -3,32 +3,41 @@
 # AUTH  bensonml
 # DATE  2024-06-21
 # DESC  a simple bash script to find files that have changed over
-#       a given date range (between refstart, refend)
-# NOTE  this script currently creates two temp files used for
-#       defining the date/time range
+#       a given date range (between STARTDATE, ENDDATE)
+# NOTE  Takes an optional date for STARTDATE, and an optional ENDDATE
+#       both in iso format YYYY-MM-DD
+#       if no STARTDATE is provided, will use today
+#       if no ENDDATE is provided, it will use 1 day after STARTDATE
+#       Uses macos/bsd date, not GNU date
 # ------------------------------------------------------------------
 
-VERSION=0.1.0
-USAGE="Usage: bash fileDateIndexer.sh > list.txt"
+VERSION=0.2.0
+USAGE="
+Usage: bash fileDateIndexer.sh
+Usage: bash fileDateIndexer.sh [YYYY-mm-dd]
+Usage: bash fileDateIndexer.sh [YYYY-mm-dd] [YYYY-mm-dd]"
 
-set -o nounset
+# read start date, if nothing passed, use today's date
+STARTDATE=$1;
+if [[ ! $STARTDATE ]]; then
+    STARTDATE=$(date +%Y-%m-%d)
+fi
 
-touch -t 202406190000.00 refstart
-touch -t 202406200000.00 refend
+# read end date, if nothing passed, use day after STARTDATE
+ENDDATE=$2
+if [[ ! $ENDDATE ]]; then
+    ENDDATE=$(date -j -v +1d -f "%Y-%m-%d" $STARTDATE +%Y-%m-%d)
+fi
+
+# header for output
+echo "# FILE INDEX FOR $STARTDATE to $ENDDATE"
 
 find $HOME -type f \
-    -not \( -path "$HOME/Library/*" \
-	        -o -path "$HOME/LOGs/iterm/*" \
-	        -o -path "$HOME/Pictures/Photos Library.photoslibrary/*" \
-	        -o -path "$HOME/\.*" \
-	        -o -wholename "*.git*" \) \
-	-newer refstart \
-    \! -newer refend \
-    \! -name refend
+	-not \( -path "$HOME/Library/*" \
+	-o -path "$HOME/LOGs/iterm/*" \
+	-o -path "$HOME/Pictures/Photos Library.photoslibrary/*" \
+	-o -path "$HOME/\.*" \
+	-o -wholename "*.git*" \) \
+	-newermt $STARTDATE \
+	\! -newermt $ENDDATE
 
-# note 1: only find where time is after refstart, aka newer than refstart 
-# note 2: only find where time is NOT after refend, aka newer than refend 
-# note 3: but the file is not itself the refend file
-# note 4: does not throw error if one of the path/dirs does not exist
-# TODO do not create/use refstart file, refend file
-# TODO use an input date (opt), and convert to appropriate syntax to work
